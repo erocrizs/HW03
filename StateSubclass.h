b6253588e9673ccebe8de35e37f7c93b95e3cd7d
erocrizs <aemielvin.loremia@gmail.com>
2016-03-06 22:40:42 +0800

    Create SplashScreen

--- StateSubclass.h
+++ StateSubclass.h
@@ -7,8 +7,15 @@ class SplashState: public State {
 private:
     StateManager* sm;
     bool is_active;
+    sf::Font titleFont;
+    sf::Font subtitleFont;
+    sf::Text titleText;
+    sf::Text subtitleText;
+    sf::Text highScore;
+    sf::Text playText;
 public:
     SplashState(StateManager* sm);
+    void onActivate(std::string pass) override;
     void handleInput(const vec2i& mouse) override;
     void update(float dt) override;
     void render(sf::RenderWindow& window) override;
